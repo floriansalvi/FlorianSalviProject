d160299a357f20c74a0decd027c6e8519bf6208b
floriansalvi <floriansalvipro@gmail.com>
2024-10-10 13:44:13 +0200

    First commit

---
+++ MyProject/src/StructureCouchDB.vue
@@ -0,0 +1,43 @@
+<script lang="ts">
+import PouchDB from 'pouchdb';
+
+export default {
+
+  data() {
+    return {
+      datas: [],
+      databaseReference: null,
+    };
+  },
+
+  methods: {
+
+    inc() {
+
+    },
+
+    initDatabase() {
+      // lancer une initialisation de la base de données
+      // Si la connexion est ok, alors je fais un fetchdata
+      const db = new PouchDB('monurl/macollection');
+
+    },
+
+    fetchData() {
+      // récupérer tous les documents de ma collection
+    }
+
+  },
+
+  mounted() {
+    this.initDatabase()
+  }
+
+}
+</script>
+
+<template>
+  <h1>InfraDon2</h1>
+  <p>Counter: {{ datas }}</p>
+  <button type="button" role="button" @click="inc">+1</button>
+</template>
